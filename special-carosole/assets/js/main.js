5647b2ce6d562d4d5f339f35d869c25ef9ce83cc
tauhid16 <33229695+tauhid16@users.noreply.github.com>
2018-02-26 17:46:35 +0600

    special-carosole

---
+++ special-carosole/assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+     $(".feature-trailor-carousel").owlcarousel({
+        items:1,
+        loop:true,
+        autoplay:false,
+        nav:false,
+        dots:true,
+
+
+        
+
+});
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
