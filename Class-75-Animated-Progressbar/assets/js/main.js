650955fb1dee2a6a66d79094e0ef50df44e58b6d
tauhid16 <33229695+tauhid16@users.noreply.github.com>
2018-05-09 00:41:35 +0600

    Class-75-Animated-Progressbar

---
+++ Class-75-Animated-Progressbar/assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#bar1').barfiller({ barColor: '#fff' });
+    $('#bar2').barfiller({ barColor: '#fff' });
+	$('#bar3').barfiller({ barColor: '#fff' });
+	$('#bar4').barfiller({ barColor: '#fff', duration: 3000 });
+   
+
+
+
+});
