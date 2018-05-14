9ae01f30f834e7d6501a05febf4d99434a8332cd
tauhid16 <33229695+tauhid16@users.noreply.github.com>
2018-05-14 07:28:24 +0600

    Class-84-Flip-Box

---
+++ Class-84-Flip-Box/assets/js/main.js
@@ -0,0 +1,36 @@
+
+	 $(document).ready(function(){
+
+    
+   
+    $('.hover').hover(function(){
+      $(this).addClass('flip');
+    },function(){
+      $(this).removeClass('flip');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+	
