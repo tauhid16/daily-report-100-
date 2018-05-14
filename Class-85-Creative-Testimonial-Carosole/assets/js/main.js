e5889ebcaec62b8b9b42fa22f2892e430b2e6816
tauhid16 <33229695+tauhid16@users.noreply.github.com>
2018-05-14 11:08:15 +0600

    Class-85-Creative-Testimonial-Carosole

---
+++ Class-85-Creative-Testimonial-Carosole/assets/js/main.js
@@ -0,0 +1,33 @@
+
+	 $(document).ready(function(){
+
+     $(".owl-carousel").owlCarousel({
+          items:1,
+          dots:false,
+          nav:true,
+          navigationText : ["<i class='fas fa-angle-right'></i>","<i class='fas fa-angle-right'></i>"],
+          loop:true,
+          autoPlay : false,
+
+
+
+     });
+   
+   
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+	
