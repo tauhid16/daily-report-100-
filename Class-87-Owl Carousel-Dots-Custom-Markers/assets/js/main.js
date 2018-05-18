5ea186c5551202979059c79eda2a128317076d10
tauhid16 <33229695+tauhid16@users.noreply.github.com>
2018-05-18 20:15:39 +0600

    Class-87-Owl Carousel-Dots-Custom-Markers

---
+++ Class-87-Owl Carousel-Dots-Custom-Markers/assets/js/main.js
@@ -0,0 +1,34 @@
+
+	 $(document).ready(function(){
+
+     $(".owl-carousel").owlCarousel({
+          items:1,
+          dots:true,
+          nav:true,
+          navigationText : ["<i class='fas fa-angle-right'></i>","<i class='fas fa-angle-right'></i>"],
+          loop:true,
+          autoPlay : false,
+         dotsData:true,
+
+
+
+     });
+   
+   
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+	
